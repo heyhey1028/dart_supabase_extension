73e4c1c8b7bd662913b9f2a5fc7b8fce4b9a9f63
heyhey1028 <shoheiogawa1028@gmail.com>
2023-12-20 22:19:58 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dart_supabase_extension
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/dart_supabase_extension/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dart_supabase_extension
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
